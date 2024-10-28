0c49c5ff83c5aff3790bc5867a7c6abb58c2d90d
Lori Ching <lching@scu.edu>
2024-10-28 16:09:43 -0700

    new commits

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main () {
   cout << "Hello World" << endl;
-  cout << "Goodbye World" << endl;
+  cout << "World" << endl;
 
   MyClass a(5);
   cout << a.getX() << endl;
